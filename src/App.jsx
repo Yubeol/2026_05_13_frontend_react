9370310ef03946c0260f806511a20218dd1f3861
Yubeol <minkyu.ryu00@gmail.com>
2026-05-14 19:18:33 +0900

    입력 제출 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import {useState} from 'react'
 
 const style = {
   width: "500px",
@@ -9,18 +10,33 @@ const style = {
   alignItems: "center"
 }
 
+const initialInfo = {
+  이름: "John",
+  국어: "",
+  영어: "",
+  수학: "",
+  과학: ""
+}
 
 function App() {
+  const [info, setInfo] = useState(initialInfo)
+
   const handleChange = (event) => {
-    console.log(event.target.name)
+    const { name, value } = event.target
+    setInfo({ ...info, [name]: value })
+  }
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(info)
   }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>이름:
         <input
           type="text"
           name="이름"
-          value=""
+          value={info.이름}
           onChange={handleChange}
         />
       </div>
@@ -28,7 +44,7 @@ function App() {
         <input
           type="number"
           name="국어"
-          value=""
+          value={info.국어}
           onChange={handleChange}
         />
       </div>
@@ -36,7 +52,7 @@ function App() {
         <input
           type="number"
           name="영어"
-          value=""
+          value={info.영어}
           onChange={handleChange}
         />
       </div>
@@ -44,7 +60,7 @@ function App() {
         <input
           type="number"
           name="수학"
-          value=""
+          value={info.수학}
           onChange={handleChange}
         />
       </div>
@@ -52,7 +68,7 @@ function App() {
         <input
           type="number"
           name="과학"
-          value=""
+          value={info.과학}
           onChange={handleChange}
         />
       </div>
